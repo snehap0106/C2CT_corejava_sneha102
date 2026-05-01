783a4cac2d9396085d23abaad93891290b4ca5b8
Sneha P <snehapsneha01@gmail.com>
2026-05-01 22:02:32 +0530

    Update SLInheritanceDemo.java

--- SLInheritanceDemo.java
+++ SLInheritanceDemo.java
@@ -4,7 +4,7 @@ package com.tnsif.inheritance.slinheritance;
 public class SLInheritanceDemo {
 
 	public static void main(String[] args) {
-		Student s1=new Student("Gayatri","123456","Bangalore",27568236,10,"SKIT");
+		Student s1=new Student("Sneha","123456","Bangalore",27568236,10,"SKIT");
 		System.out.println(s1);
 
 	}
