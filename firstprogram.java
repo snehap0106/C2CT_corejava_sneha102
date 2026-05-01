a4430f608762c7c732fd38d169f6c61d58639ea9
Sneha P <snehapsneha01@gmail.com>
2026-05-01 21:44:45 +0530

    Add files via upload

---
+++ firstprogram.java
@@ -0,0 +1,17 @@
+
+package com.tnsif.basics;
+
+import com.skit.basics.Student;
+
+
+  class FirstProgram extends Student {
+
+	public static void main(String[] args) {
+	    
+		System.out.println("Hello World");
+		Student s3=new Student();
+		s3.sname="ABC";
+		
+	}
+
+	}
